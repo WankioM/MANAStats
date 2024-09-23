10120c42d25bd0f6e06e2b904364e3026b9e75d3
WankioM <tracywankio29@gmail.com>
2024-09-23 20:38:50 +0300

    apollo changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage';
 import LeaderBoard from './components/LeaderBoard';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.studio.thegraph.com/query/89211/decentragraph/version/latest', // Replace with your subgraph URL
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
       <LandingPage />
       <LeaderBoard />
       
     </div>
+    </ApolloProvider>
   );
 }
 
--- frontend/src/components/LeaderBoard.js
+++ frontend/src/components/LeaderBoard.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import './LeaderBoard.css'; 
 import PodiumCard from './PodiumCard';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_BID_SUCCESSFULS = gql`
+  query {
+    bidSuccessfuls(first: 15, orderBy: _pricePerLandInMana) {
+      id
+      _pricePerLandInMana
+      _manaAmountToBurn
+      _token
+      _xs
+      _ys
+      _beneficiary
+    }
+  }
+`;
+
+
 const LeaderBoard = () => {
+
+  const { loading, error, data } = useQuery(GET_BID_SUCCESSFULS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  
   return (
     <div className="leaderboard">
       <p> Leaderboard </p>
