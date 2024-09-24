23365dea973cc582e8a400b5dd94654f4bb36c5a
njeriwangumo <njeriwangumo@gmail.com>
2024-09-24 13:38:48 +0300

    fontmerge

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -4,7 +4,11 @@ import './LandingPage.css';
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <p> ManaStat Subgraph Indexing </p>
+    <div className='heading1'>
+      <p> Understanding Auctions Has Never Been Easier</p>
+      </div>
+      <div className='heading2'><p> We help you retrive real estate data on chain within seconds,so you can target the right bidders.</p></div>
+      
     </div>
   );
 };
