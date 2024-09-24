d21a89b0771bb01c34997836fc5c7ef3b716da4f
WankioM <tracywankio29@gmail.com>
2024-09-24 14:40:53 +0300

    card body wrap text edit

--- frontend/src/components/PodiumCard.js
+++ frontend/src/components/PodiumCard.js
@@ -6,6 +6,7 @@ const PodiumCard = ({ bid, position }) => {
   return (
     <div className="card podiumcard">
       <div className="card-header">
+
         <img 
           src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Trophy-and-Medals-PNG/Gold_Cup_Transparent_Image.png?m=1629819139"
           alt="Position Icon"
@@ -20,7 +21,7 @@ const PodiumCard = ({ bid, position }) => {
       </div>
 
       <div className="card-body">
-        <h3 className="hbidder">Bidder: {bid._beneficiary}</h3>
+        <p className="hbidder">Bidder: {bid._beneficiary}</p>
           <div className="info">
             <div className="bid-price">
             <img 
@@ -35,9 +36,10 @@ const PodiumCard = ({ bid, position }) => {
             </div>
             
           <div className="location">
-            <FaMapMarkerAlt style={{ color: 'white', fontSize: '20px' }} />
+            <FaMapMarkerAlt style={{ color: 'white', fontSize: '18px' }} />
             <span className="hlocation"> ({bid._xs[0]}, {bid._ys[0]})</span>
           </div>
+
         </div>
       </div>
     </div>
